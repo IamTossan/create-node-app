e9e7953cf72ba0fd42dca590666afe931f641ce6
Albert Oudompheng <tossanmusic@gmail.com>
2020-05-16 19:50:56 +0200

    feat: update minimal node required version to 10.12.0

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,16 @@
 
 var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
-var major = semver[0];
+var major = semver[0]
+var minor = semver[1]
 
-if (major < 10) {
+if (Number(major) < 10 || (Number(major) === 10 && Number(minor) < 12)) {
   console.error(
     'You are running Node ' +
-      currentNodeVersion +
-      '.\n' +
-      'Create Node App requires Node 10 or higher. \n' +
-      'Please update your version of Node.',
+    currentNodeVersion +
+    '.\n' +
+    'Create Node App requires Node 10.12.0 or higher. \n' +
+    'Please update your version of Node.',
   );
   process.exit(1);
 }

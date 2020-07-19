157c9b8a0ad605aaa0b921ce56a90887e55a4d32
Albert Oudompheng <tossanmusic@gmail.com>
2020-07-19 14:38:13 +0200

    feat: update dependencies

--- config/packages.js
+++ config/packages.js
@@ -1,27 +1,27 @@
 const { testEnum } = require('../shared/enums');
 
 const basePackages = [
-  'typescript@3.9.2',
-  'ts-node-dev@1.0.0-pre.44'
+  'typescript@3.9.7',
+  'ts-node-dev@1.0.0-pre.52'
 ];
 
 const lintPackages = [
-  'eslint@7.0.0',
+  'eslint@7.5.0',
   'prettier@2.0.5',
-  '@typescript-eslint/eslint-plugin@2.33.0',
-  '@typescript-eslint/parser@2.33.0',
-  'eslint-config-airbnb-typescript',
+  '@typescript-eslint/eslint-plugin@2.34.0',
+  '@typescript-eslint/parser@2.34.0',
+  'eslint-config-airbnb-typescript@9.0.0',
   'eslint-config-prettier@6.11.0',
-  'eslint-plugin-import@2.20.2',
+  'eslint-plugin-import@2.22.0',
 ];
 
 const testPackages = {
   [testEnum.NONE]: [],
   [testEnum.JEST]: [
-    'jest@26.0.1',
-    '@types/jest@25.2.2',
-    'ts-jest@26.0.0',
-    'eslint-plugin-jest@23.13.0'
+    'jest@26.1.0',
+    '@types/jest@26.0.5',
+    'ts-jest@26.1.3',
+    'eslint-plugin-jest@23.18.0'
   ],
 };
 

069695bf595c1eda8f5543508a74aef938a5c6d2
Albert Oudompheng <tossanmusic@gmail.com>
2021-02-01 16:42:11 +0100

    feat: update package versions

--- config/packages.js
+++ config/packages.js
@@ -1,24 +1,24 @@
 const basePackages = [
-  'typescript@4.0.3',
-  'ts-node-dev@1.0.0',
-  '@types/node@14.14.3',
+  'typescript@4.1.3',
+  'ts-node-dev@1.1.1',
+  '@types/node@14.14.22',
 ];
 
 const lintPackages = [
-  'eslint@7.12.0',
-  'prettier@2.1.2',
-  '@typescript-eslint/eslint-plugin@4.5.0',
-  '@typescript-eslint/parser@4.5.0',
+  'eslint@7.19.0',
+  'prettier@2.2.1',
+  '@typescript-eslint/eslint-plugin@4.14.1',
+  '@typescript-eslint/parser@4.14.1',
   'eslint-config-airbnb-typescript@12.0.0',
-  'eslint-config-prettier@6.14.0',
+  'eslint-config-prettier@7.2.0',
   'eslint-plugin-import@2.22.1',
 ];
 
 const testPackages = [
-  'jest@26.6.1',
-  '@types/jest@26.0.15',
-  'ts-jest@26.4.2',
-  'eslint-plugin-jest@24.1.0',
+  'jest@26.6.3',
+  '@types/jest@26.0.20',
+  'ts-jest@26.5.0',
+  'eslint-plugin-jest@24.1.3',
 ];
 
 module.exports = {
